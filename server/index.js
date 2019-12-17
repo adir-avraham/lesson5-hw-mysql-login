5e77102732c35262b83bff7c218cb99377df67c0
adir-avraham <adir.avraham@gmail.com>
2019-12-17 12:21:53 +0200

    add-server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+//const authRouter = require("./routes/auth");
+const cors = require("cors");
+
+const app = express();
+
+
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+//app.use("/auth", authRouter) 
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is listening to port: " + process.env.PORT);
+})
